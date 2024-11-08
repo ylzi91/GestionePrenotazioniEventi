f8629014f4a8505ecb10dd0677cebbacc66d8842
ylzi91 <97354085+ylzi91@users.noreply.github.com>
2024-11-08 09:51:40 +0100

    Initial commit

---
+++ src/main/java/yurilenzi/GestionePrenotazioniEventi/GestionePrenotazioniEventiApplication.java
@@ -0,0 +1,13 @@
+package yurilenzi.GestionePrenotazioniEventi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionePrenotazioniEventiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionePrenotazioniEventiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yurilenzi/GestionePrenotazioniEventi/GestionePrenotazioniEventiApplicationTests.java
@@ -0,0 +1,13 @@
+package yurilenzi.GestionePrenotazioniEventi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionePrenotazioniEventiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
